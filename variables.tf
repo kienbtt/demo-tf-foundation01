c9dd7d6f94a94799bec31025f6a6b0ce53cdb702
vagrant <vagrant@kind>
2021-10-24 02:36:44 +0000

    test

--- variables.tf
+++ variables.tf
@@ -17,8 +17,10 @@ variable "cidr_block" {
 
 variable "private_subnets" {
   type = list(any)
+  default = [ "10.0.10.0/24", "10.0.11.0/24", "10.0.12.0/24" ]
 }
 
 variable "public_subnets" {
   type = list(any)
+  default = [ "10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24" ]
 }
